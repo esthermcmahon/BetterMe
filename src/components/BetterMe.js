b1bcc9d7febef0776db6bc75df31424232d171a8
estherviolin <esthernaomi.violin@gmail.com>
2020-09-11 16:06:13 -0500

    register and login complete

---
+++ src/components/BetterMe.js
@@ -0,0 +1,25 @@
+import React from "react"
+import {Route, Redirect} from "react-router-dom"
+import {ApplicationViews} from "./ApplicationViews"
+//add navbar
+import {Login} from "./auth/Login"
+import {Register} from "./auth/Register"
+
+export const BetterMe = () => (
+    <>
+        <Route render={()=> {
+            if (localStorage.getItem("BetterMe__user")) {
+
+            return (
+                <>
+                <Route render={props => <ApplicationViews {...props} />} />
+                </>
+            )
+        } else {
+            return <Redirect to="/login" />
+        }
+        }} />
+        <Route path="/login" render={props => <Login {...props} />} />
+        <Route path="/register" render={props => <Register {...props} />} />
+    </>
+)
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from "react-router-dom"
 import './index.css';
-import App from './App';
+import {BetterMe} from './components/BetterMe';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <BetterMe />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
