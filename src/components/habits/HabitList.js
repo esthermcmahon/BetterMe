11cdf8d98fbc6cc85870a01eb0a7d0cf0b69b4d6
estherviolin <esthernaomi.violin@gmail.com>
2020-09-12 12:18:38 -0500

    basic habit list rendering after reg/login

---
+++ src/components/habits/HabitList.js
@@ -0,0 +1,20 @@
+import React, { useContext, useEffect } from "react"
+import { HabitContext } from "./HabitProvider"
+import { Habit } from "./Habit"
+
+export const HabitList = () => {
+    const { habits, getHabits } = useContext(HabitContext)
+
+    useEffect(() => {
+        getHabits()
+    }, [])
+
+    return (
+        <div className="habits">
+            {
+                habits.map(habit => <Habit key={habit.id} habit={habit} />)
+
+            }
+        </div>
+    )
+}
