3fe5718b328d20df76978d705512df0b233c6036
estherviolin <esthernaomi.violin@gmail.com>
2020-09-16 15:25:37 -0500

    color string rendering array.length # of times

--- src/components/habitReps/SingleHabitProgress.js
+++ src/components/habitReps/SingleHabitProgress.js
@@ -1,16 +1,19 @@
 import React, { useContext, useEffect, useState } from "react"
 import { HabitRepContext } from "./HabitRepProvider"
+import { ColorContext } from "../colors/ColorProvider"
 
 
 
 export const SingleHabitProgress = ({ habit }) => {
-
+    const { colors, getColors } = useContext(ColorContext)
     const { habitReps, getHabitReps } = useContext(HabitRepContext)
 
+    const [ relatedColor, setRelatedColor ] = useState({})
     const [filteredHabitReps, setFilteredHabitRep] = useState([])
 
     useEffect(() => {
         getHabitReps()
+        getColors()
 
     }, [])
 
@@ -20,10 +23,33 @@ export const SingleHabitProgress = ({ habit }) => {
 
     }, [habitReps])
 
+    useEffect(() => {
+        const matchingColor = colors.find(color => habit.colorId === color.id) || {}
+        setRelatedColor(matchingColor)
+    }, [colors])
+
+    const showColors = (string, num) => {
+        if (num < 1) {
+            return ""
+        } else if (num === 1) {
+            return string
+        } else {
+            return string + showColors(string, num - 1)
+        }
+
+    }
+
 
     return (
-        <h3>{habit.name}: {filteredHabitReps.length} </h3>
+        <section className="singleHabitProgress">
+        <h3>{habit.name}: {filteredHabitReps.length}</h3>
+            
+                <div> {showColors(relatedColor.color, filteredHabitReps.length)}</div>
+            
+        </section>
+        
     )
+   
 }
 
 
