a175458becd9d422f39885df83e757e50b77f22e
estherviolin <esthernaomi.violin@gmail.com>
2020-09-16 16:12:44 -0500

    working on ternary to render only archived habits

---
+++ src/components/habits/ArchivedHabits.js
@@ -0,0 +1,28 @@
+import React, { useContext, useEffect, useState } from "react"
+import { HabitContext } from "./HabitProvider"
+
+
+
+export const HabitList = (props) => {
+    const { habits, getHabits } = useContext(HabitContext)
+
+    const [habit, setHabit] = useState({ name: "" })
+
+    const isArchived = habit.archive === true
+
+    useEffect(() => {
+        getHabits()
+    }, [])
+
+
+    return (
+        <>
+
+            <section className="archivedHabits">
+                <h3>Saved For Later </h3>
+                {isArchived ?
+                    <div>{habit.name}</div>}
+            </section>
+        </>
+    )
+                }
