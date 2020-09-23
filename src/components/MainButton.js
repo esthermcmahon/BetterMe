7df5a66e23f1d599ca9b7c502beefded1ab1cb52
estherviolin <esthernaomi.violin@gmail.com>
2020-09-23 10:54:16 -0500

    swapped material UI buttons, labels for sections, header background color, input border radius, more icon next to habit name

--- src/components/MainButton.js
+++ src/components/MainButton.js
@@ -1,12 +1,12 @@
 import React from "react"
-import DashboardIcon from '@material-ui/icons/Dashboard';
+import HomeOutlinedIcon from '@material-ui/icons/HomeOutlined';
 import "../index.css"
 
 
 export const Main = (props) => {
     return (
         <button className="nav main" onClick={() => props.history.push("/main")} title="Main">
-            <DashboardIcon />
+            <HomeOutlinedIcon />
         </button>
     )
 }
--- src/components/habits/HabitList.js
+++ src/components/habits/HabitList.js
@@ -5,24 +5,28 @@ import { Habit } from "./Habit"
 
 export const HabitList = (props) => {
     const { habits, getHabits } = useContext(HabitContext)
-   
+
     useEffect(() => {
         getHabits()
     }, [])
 
     const filteredHabits = habits.filter(habit => habit.userId === parseInt(localStorage.getItem("BetterMe__user"))) || {}
-    
+
 
     return (
-        <div className="habits">
-            {
-                filteredHabits.map(habit => {
-                
-                    return habit.archive === false ? <Habit key={habit.id} habit={habit} {...props} /> : ""
-                })
+        <section>
+            <h3>Habits</h3>
+            <div className="habits">
 
-            }
-        </div>
+                {
+                    filteredHabits.map(habit => {
+
+                        return habit.archive === false ? <Habit key={habit.id} habit={habit} {...props} /> : ""
+                    })
+
+                }
+            </div>
+        </section>
     )
 }
 
