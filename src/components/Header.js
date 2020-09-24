ba4e24fd4f94a3a2d69bfb671238e04483bfb0f3
estherviolin <esthernaomi.violin@gmail.com>
2020-09-24 09:33:01 -0500

    welcome, user renders on main

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,15 @@
-import React, { useEffect, useState, useContext } from "react"
+import React from "react"
 import "./header.css"
-import { UserContext } from "./users/UserProvider"
-
-
-export const Header = (props) => {
-   const { users, getUsers } = useContext(UserContext)
-
-   const [user, setUser] = useState({})
-
-   useEffect(() => {
-      getUsers()
-   }, [])
 
 
 
-   useEffect(() => {
-      const getUserName = users.find(user => user.id === parseInt(localStorage.getItem("BetterMe__user"))) || {}
-      setUser(getUserName)
-   }, [user])
-
-   // const loggedOut = props.match.params.hasOwnProperty("login") || props.match.params.hasOwnProperty("register")
-
+export const Header = () => {
 
    return (
     <header className="siteHeaderContainer">
     <h1 className="siteHeader">BetterMe</h1>
-    {/* <p>Welcome, {user.name}</p> */}
     </header>  
    )
    
 }
 
-// {loggedOut ? "" : <p>Welcome, {user.name}</p> }
